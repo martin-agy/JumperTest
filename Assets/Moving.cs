fa49a2c686ec5b84b993923651ad475345dc4b93
martin-agy <martin.lubcke@arlandagymnasiet.se>
2020-10-11 16:22:03 +0200

    Styling and changed to mouse click for creating blocks

--- Assets/Moving.cs
+++ Assets/Moving.cs
@@ -6,14 +6,14 @@ public class Moving : MonoBehaviour
     public float RunSpeed = 1;
     public float StrafeSpeed = 1;
     public float Gravity = 10f;
-    public GameObject Block;
+    public GameObject[] Blocks;
 
     CharacterController characterController;
     Camera headCamera;
     Vector3 velocity = Vector3.zero;
     float bodyAngle = 0f; // Turning body left/right
     float headAngle = 0f; // Turning head up/down
-
+    int blockIndex = 0;
 
     private void Start()
     {
@@ -23,6 +23,15 @@ public class Moving : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+
+
         // Find rotation from mouse 
         bodyAngle += 10f * Input.GetAxis("Mouse X");
         headAngle -= 10f * Input.GetAxis("Mouse Y");
@@ -67,22 +76,32 @@ public class Moving : MonoBehaviour
         Vector3 worldVelocity = transform.TransformVector(velocity);
         characterController.Move(worldVelocity * Time.deltaTime);
 
+        // Look through button 1 to 9 to see if clicked
+        // If there are as many blocks: use it
+        for (int i = 1; i <= 9; i++)
+            if (Input.GetKey(i.ToString()))
+                if (Blocks.Length >= i)
+                    blockIndex = i - 1;
+
         RaycastHit hit;
         int layerMask = 1 << 8;
         layerMask = ~layerMask;
         // If we have a block straight in front of the camera
         if (Physics.Raycast(headCamera.transform.position, headCamera.transform.forward, out hit, 100, layerMask))
         {
-            // Create a block assuming the hit is a block
-            if (Input.GetKeyDown(KeyCode.X))
-                Instantiate<GameObject>(Block, hit.transform.position + hit.normal, hit.transform.rotation);
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Create a block according to hit point and normal
+                if (Input.GetKey(KeyCode.LeftControl))
+                    Instantiate<GameObject>(Blocks[blockIndex], hit.point + 0.5f * hit.normal, Quaternion.LookRotation(hit.normal, Vector3.up));
+                // Create a block assuming the hit is a block
+                else
+                    Instantiate<GameObject>(Blocks[blockIndex], hit.transform.position + hit.normal, hit.transform.rotation);
+            }
 
-            // Create a block according to hit point and normal
-            if (Input.GetKeyDown(KeyCode.C))
-                Instantiate<GameObject>(Block, hit.point + 0.5f * hit.normal, Quaternion.LookRotation(hit.normal, Vector3.up));
 
             // Destroy the hit block
-            if (Input.GetKeyDown(KeyCode.Z))
+            if (Input.GetMouseButtonDown(1))
                 Destroy(hit.transform.gameObject);
         }
     }
